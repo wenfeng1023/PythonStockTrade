ffb40ac87868203301cf7fd75004dfca90d4989d
HyoSeung Lee <godinus12@gmail.com>
2021-12-15 17:34:33 +0900

    ff

--- FinancialDataAnalysisWithPython/05 - Pandas Datareader.py
+++ FinancialDataAnalysisWithPython/05 - Pandas Datareader.py
@@ -2,6 +2,31 @@
 # - https://pandas-datareader.readthedocs.io/en/latest/
 
 import pandas_datareader as pdr
+import datetime as dt
+
+ticker = "AAPL"
+start = dt.datetime(2019, 1, 1)
+end = dt.datetime(2020, 12, 31)
+
+data = pdr.get_data_yahoo(ticker, start, end)
+
+print( data.head() )
+print( data.index )
+print( data.dtypes )
+print( data.tail() )
+data2 = pdr.get_data_stooq(ticker, start)
+print( data2.head() )
+print( data2.tail() )
+nasdaq_sym = pdr.get_nasdaq_symbols()
+print( nasdaq_sym.loc['AAPL'] )
+print( len(nasdaq_sym) )
+
+
+
+
+
+
+
 
 
 
