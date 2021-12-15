61d5ccb36bb317df8ffd517d44c896556886527a
HyoSeung Lee <godinus12@gmail.com>
2021-12-15 19:10:46 +0900

    gfdsa

---
+++ MachineLearningWithPython/01 - Intro to NumPy.py
@@ -0,0 +1,87 @@
+# # Using Pandas and NumPy
+# ## Portfolio and Risk
+# - Pandas Datareader
+# - Pandas
+# - Numpy
+# - Portfolio and return
+
+# ### Resources
+# - Pandas Datareader
+# https://pandas-datareader.readthedocs.io/ (https://youtu.be/sgndYho8RyI)
+# - Pandas https://pandas.pydata.org (https://youtu.be/m8ahf_c9hEc)
+# - NumPy http://numpy.org (Focus here)
+
+### Pandas Datareader & Pandas DataFrame
+
+import numpy as np
+import pandas_datareader as pdr
+import datetime as dt
+import pandas as pd
+
+start = dt.datetime(2020, 1, 1)
+data = pdr.get_data_yahoo("AAPL", start)
+
+print( data.head() )
+print( data.index )
+print( data.dtypes )
+print( type(data) )
+
+### NumPy
+
+data.to_numpy()
+arr = data.to_numpy()
+print( arr.shape )
+print( len(data) )
+print( arr[0] )
+print( data.head(1) )
+print( arr.dtype )
+small = arr[:10, 0].copy()
+print( small )
+print( data.head() )
+print( np.max(small) )
+print( small.max() )
+print( small.argmax() )
+print( small )
+print( small[small.argmax()] )
+print( np.log(small) )
+print( np.log(data) )
+
+print( data/data.shift() )
+print( data.head() )
+print( 75.144997/75.150002 )
+print( np.sum(np.log(data/data.shift())) )
+print( np.log(data/data.iloc[0]).tail(1) )
+print( small.shape )
+print( small )
+print( small.reshape(2, 5) )
+print( small.reshape(10, 1) )
+print( small.reshape(-1, 1) )
+
+### Portfolios
+tickers = ['AAPL', 'MSFT', 'TWTR', 'IBM']
+start = dt.datetime(2020, 1, 1)
+
+data = pdr.get_data_yahoo(tickers, start)
+print( data.head() )
+data = data['Adj Close']
+print( data.head() )
+portfolios = [.25, .15, .40, .20]
+print( np.sum(portfolios) )
+print( (data/data.iloc[0])*portfolios*100000 )
+
+weight = np.random.random(4)
+weight /= weight.sum()
+print( weight )
+
+
+print( weight.sum() )
+
+np.sum((data/data.iloc[0])*portfolios*100000, axis=1)
+
+np.sum((data/data.iloc[0])*weight*100000, axis=1)
+
+
+
+
+
+
